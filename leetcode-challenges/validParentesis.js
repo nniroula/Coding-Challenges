e1520432acd0e58ab08cfa234f380a5b96daf127
nabin <nkbhaie2017@gmail.com>
2023-03-07 18:38:25 -0700

    add logic

--- leetcode-challenges/validParentesis.js
+++ leetcode-challenges/validParentesis.js
@@ -26,10 +26,12 @@ Constraints:
     s consists of parentheses only '()[]{}'.
  */
 
-    /**
+/**
  * @param {string} s
  * @return {boolean}
  */
+
+
 var isValid = function(s) {
     // You cannot remove char from string, so convert string to an array and then join them back later to 
     // revert to string
@@ -39,15 +41,11 @@ var isValid = function(s) {
     let count = 0;
     while(count < stringToArray.length){
         let firstItem = stringToArray[0];
-        // console.log(firstItem);
         let lastIndexOfFirstItem = stringToArray.lastIndexOf(firstItem); // this
-        // console.log(`Last index is ${lastIndexOfFirstItem}`);
-    
         let slicedArray = stringToArray.slice(lastIndexOfFirstItem + 1);
-        // console.log(slicedArray);
         let firstIndexOfOppositeParen = 0;
 
-        if(firstItem === '('){
+        if(firstItem === '('){  // "([)]"
             firstIndexOfOppositeParen = slicedArray.indexOf(')');  // this, indexof returns -1 if not found
             if(firstIndexOfOppositeParen === -1){
                 boolValue = false;
@@ -61,7 +59,6 @@ var isValid = function(s) {
             stringToArray.splice(lastIndexOfFirstItem, 1);
         } 
         else if(firstItem === '{'){
-        // if(firstItem === '{'){
             console.log(slicedArray);
             firstIndexOfOppositeParen = slicedArray.indexOf('}');  // this
             if(firstIndexOfOppositeParen === -1){
@@ -69,27 +66,17 @@ var isValid = function(s) {
                 return boolValue;
             }
             let actualIndexInStringS = lastIndexOfFirstItem + firstIndexOfOppositeParen + 1; // this
-            // console.log(`Actual index is ${actualIndexInStringS}`);
-            // console.log("line 70: ...");
-            // console.log(stringToArray);
-            // stringToArray.pop(actualIndexInStringS);
             stringToArray.splice(actualIndexInStringS, 1);
-            // stringToArray.pop(lastIndexOfFirstItem);
-            // stringToArray.shift(lastIndexOfFirstItem);
             stringToArray.splice(lastIndexOfFirstItem, 1);
         }
         else if(firstItem === '['){
-        // if(firstItem === '['){
             firstIndexOfOppositeParen = slicedArray.indexOf(']');  // this
             if(firstIndexOfOppositeParen === -1){
                 boolValue = false;
                 return boolValue;
             }
             let actualIndexInStringS = lastIndexOfFirstItem + firstIndexOfOppositeParen + 1; // this
-            // stringToArray.pop(actualIndexInStringS);
             stringToArray.splice(actualIndexInStringS, 1);
-            // stringToArray.pop(lastIndexOfFirstItem);
-            // stringToArray.shift(lastIndexOfFirstItem);
             stringToArray.splice(lastIndexOfFirstItem, 1);
         }else {
             boolValue = false;
@@ -110,8 +97,60 @@ let s4 = "(]";  // false
 let s5 = "((({{[}}))"; // false
 let s6 = "(]}{])";  // false
 let s7 = "{[]}";
+let s8 = "([)]";   // false per leetcode, per me its true
 
-console.log("Results ..... ");
+// console.log("Results ..... ");
+// console.log(isValid(s1)); // true
+// console.log(isValid(s2));  // true
+// console.log(isValid(s3));  // false
+// console.log(isValid(s4));    // false
+// console.log(isValid(s5));
+// console.log(isValid(s6));
+// console.log(isValid(s7));
+console.log(isValid(s8));
+
+
+
+// for leetcode purpose
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+/*
+var isValid = function(s) {
+    // You cannot remove char from string, so convert string to an array and then join them back later to 
+    // revert to string
+
+    // for leetcode let s8 = "([)]";   is false, s7 = "{[]}" is true
+    let trueOrFalse = false;
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === "(" && s[i+1] === ")"){
+            i++;
+            trueOrFalse = true;
+        }else if(s[i] === "{" && s[i+1] === "}"){
+            i++;
+            trueOrFalse = true;
+        }else if(s[i] === "[" && s[i+1] === "]"){
+            i++;
+            trueOrFalse = true;
+        }else{
+            trueOrFalse = false;
+        }
+    }
+    return trueOrFalse;
+};
+
+let s1 = "()";
+let s2 = "()[]{}";
+let s3 = ")(";  // false
+let s4 = "(]";  // false
+let s5 = "((({{[}}))"; // false
+let s6 = "(]}{])";  // false
+let s7 = "{[]}";    // true
+let s8 = "([)]";   // false per leetcode, per me its true
+
+// console.log("Results ..... ");
 console.log(isValid(s1)); // true
 console.log(isValid(s2));  // true
 console.log(isValid(s3));  // false
@@ -119,3 +158,6 @@ console.log(isValid(s4));    // false
 console.log(isValid(s5));
 console.log(isValid(s6));
 console.log(isValid(s7));
+console.log(isValid(s8));
+
+*/
